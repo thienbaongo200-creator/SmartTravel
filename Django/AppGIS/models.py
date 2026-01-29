e627d94e447829c38bf5abd51ad73c94f01c99d0
thienbaongo200-creator <thienbaongo200@gmail.com>
2026-01-29 23:23:38 +0700

    models.py
    Signed-off-by: thienbaongo200-creator <thienbaongo200@gmail.com>

--- Django/AppGIS/models.py
+++ Django/AppGIS/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class TodoItem(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+    def __str__(self): 
+        return self.title 
+    
+class TourismPoint(models.Model): 
+    name = models.CharField(max_length=200) 
+    description = models.TextField(blank=True) 
+    latitude = models.FloatField() 
+    longitude = models.FloatField() 
+    type = models.CharField(max_length=100, blank=True) 
+    
+    def __str__(self): 
+        return self.name
