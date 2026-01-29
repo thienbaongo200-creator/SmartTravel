a82d3b7d4e2bc6ec24d8b8977e766acb371aaabb
thienbaongo200-creator <thienbaongo200@gmail.com>
2026-01-29 22:07:24 +0700

    urls.py

--- Django/AppGIS/views.py
+++ Django/AppGIS/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
 
 # Create your views here.
-def home(requets):
-    return HttpResponse("Hello world")
-
+def index(requets):
+    return render(requets, 'home.html')
+def about(requets):
+    return render(requets, 'about.html')
+def destination(requets):
+    return render(requets, 'destination.html')
+def events(request):
+    return render(request, 'events.html')
+def services(request):
+    return render(request, 'services.html')
+def contact(request):
+    return render(request, 'contact.html')
