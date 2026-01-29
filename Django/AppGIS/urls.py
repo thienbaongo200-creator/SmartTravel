c55cc8c61d1c4d786126bd24acf5f62b227e34f0
thienbaongo200-creator <thienbaongo200@gmail.com>
2026-01-29 22:07:24 +0700

    urls.py

--- Django/AppGIS/urls.py
+++ Django/AppGIS/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path('', views.index, name='home'), 
+    path('about/', views.about, name='about'),
+    path('destinations/', views.destinations, name='destinations'),
+    path('events/', views.events, name='events'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
