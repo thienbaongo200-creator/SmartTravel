5483a18f0dc1d39049b3189f3cac499f0cdbb93e
Chien <ngminhchien25@gmail.com>
2026-02-03 14:37:46 +0700

    Update services/hotels
    Signed-off-by: Chien <ngminhchien25@gmail.com>

--- Django/AppGIS/urls.py
+++ Django/AppGIS/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'), 
-    path('about/', views.about, name='about'),
-    path('destinations/', views.destinations, name='destinations'),
-    path('events/', views.events, name='events'),
-    path('services/', views.services, name='services'),
-    path('contact/', views.contact, name='contact'),
+    path("", views.index, name="home"),
+    path("about/", views.about, name="about"),
+    path("destinations/", views.destinations, name="destinations"),
+    path("contact/", views.contact, name="contact"),
+    path("events/", views.events, name="events"),
+    path("services/", views.services, name="services"),
+    path("hotels/", views.hotels_list, name="hotels"),
 
-    #Tool WebGIS
-    path('search/', views.search, name='search'),
-    path('distance/', views.distance, name='distance'),
+    # Tool WebGIS
+    path("search/", views.search, name="search"),
+    path("distance/", views.distance, name="distance"),
     path("distance/<int:point_id>/", views.get_distance, name="get_distance"),
 ]
