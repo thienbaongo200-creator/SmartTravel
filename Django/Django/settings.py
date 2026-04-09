87ea5b2e41c32517210fa5d591a06469bdc62c44
Chien <ngminhchien25@gmail.com>
2026-04-09 12:14:53 +0700

    Fix 404, 403
    Signed-off-by: Chien <ngminhchien25@gmail.com>

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8$0e3ly5@n2jf=r^1*3j^wcp3qt4fr(9$v@ify%1qq0(n0a=#a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
