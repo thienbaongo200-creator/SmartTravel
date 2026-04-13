8c67a1113da5ede28b81ba5f054b0ecc902fabea
thienbaongo200-creator <thienbaongo200@gmail.com>
2026-04-13 11:58:09 +0700

    Mô tả

--- Django/AppGIS/serializers.py
+++ Django/AppGIS/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
-from .models import TourBooking
+from .models import TourBooking, Category, TourismPoint
 
 class TourBookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = TourBooking
         fields = "__all__"
+
+class TourismPointSerializer(serializers.ModelSerializer):
+    # Lấy tên category thay vì ID để hiển thị đẹp hơn
+    category = serializers.SlugRelatedField(
+        slug_field='name', 
+        queryset=Category.objects.all(),
+        allow_null=True,
+        required=False
+    )
+
+    class Meta:
+        model = TourismPoint
+        # Liệt kê đầy đủ để chắc chắn description xuất hiện trong JSON
+        fields = [
+            'id', 'name', 'description', 'latitude', 'longitude', 
+            'category', 'address', 'price', 'img', 'gallery', 'rating'
+        ]
